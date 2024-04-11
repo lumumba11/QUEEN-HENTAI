21513097fe2df3f23877171339bebd0daceb3da4
lumumba11 <158476361+lumumba11@users.noreply.github.com>
2024-04-12 01:31:53 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+  ['+254 742233661', 'lumumba', true],
+  ['+254 742233661', 'lumumba', true], 
   [''] 
 ] //Number of owners
 
